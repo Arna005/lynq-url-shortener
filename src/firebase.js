79d8a762ef2d4d481901224adc0e7c57a08fc917
Arina Naveed <arinanaveed@Arinas-Air.lan>
2025-05-13 00:01:50 +0200

    fix: secure API key restrictions

--- src/firebase.js
+++ src/firebase.js
@@ -2,11 +2,11 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
+// Always use environment variables!
 const firebaseConfig = {
-  apiKey: "AIzaSyCGtzqPpUq66bwePQwf0OPEuvujPhMx-RE",
-  authDomain: "lynq-7121d.firebaseapp.com",
-  projectId: "lynq-7121d",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: "lynq-7121d", 
   storageBucket: "lynq-7121d.appspot.com",
   messagingSenderId: "763996853742",
   appId: "1:763996853742:web:c3c35c81311e2c55c62aff"
@@ -14,7 +14,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Export Firestore and Auth services
 export const db = getFirestore(app);
 export const auth = getAuth(app);
